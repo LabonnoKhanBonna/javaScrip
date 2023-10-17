f3ff369f50dcacfa9c5ef9b50cf5f674dcf8d894
Labonno Khan Bonna <bonnalabonnokhan2708@gmail.com>
2023-10-17 22:48:17 +0600

    my second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hi my name is khan and i am not a terrist");
+var x =10;
+console.log(x);
+var age = 25; //number
+var name = 'labonno khan bonna'; // string 
+var y = true; // boolean
+var z = 25.4565
+var p = parseInt(z);
+console.log(p);
